fc16dd88d8a3fd4e49eef1b811cb07063ec7c012
erik <westley_mak@yahoo.com>
2014-04-24 04:35:18 -0500

    Cleaned up amazon file view. Hide links when downloading from S3

--- finalproject_242/settings/settings_base.py
+++ finalproject_242/settings/settings_base.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.amazon',
     'allauth.socialaccount.providers.google',
     'website',
-    'amazon',
+    'service_manager',
     'accounts',
     'dropbox',
 )
